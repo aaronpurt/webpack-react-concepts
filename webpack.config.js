5678691045caf2f5a4ebb0333463c175a5066ad6
aaron <apurtill@hollandamericagroup.com>
2018-09-29 18:25:08 -0700

    add updated webpack config to include HtmlWebpack Plugin commands

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+// template: value template where looking for my html file. 
+// filename: value is the name of the minified html file generated in dist folder
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
-      ]
-    }
-  };
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
